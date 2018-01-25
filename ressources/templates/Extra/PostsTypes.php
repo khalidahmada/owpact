6a52014f9c83e3209da9af13b7a1421072a70395
Khalid ahmada <khalidoahmada@gmail.com>
2018-01-25 17:31:43 +0000

    Add icons to icons Posts type

--- ressources/templates/Extra/PostsTypes.php
+++ ressources/templates/Extra/PostsTypes.php
@@ -1,4 +1,9 @@
 <?php
+    /*
+     * To change Icons please see
+     * @see https://developer.wordpress.org/resource/dashicons/
+     */
+
     // Register Post Types for Project
     // Exemple
     //registerPostType('tuto','Tutos','dashicons-megaphone');
